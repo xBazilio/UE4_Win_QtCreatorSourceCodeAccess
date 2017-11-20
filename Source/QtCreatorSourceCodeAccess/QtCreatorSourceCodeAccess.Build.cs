6b9434f9e1c09ee62c4b5687dae48e6983010b8b
Bazilio <x.bazilio@gmail.com>
2017-11-20 19:27:45 +0000

    opted-in to IWYU

--- Source/QtCreatorSourceCodeAccess/QtCreatorSourceCodeAccess.Build.cs
+++ Source/QtCreatorSourceCodeAccess/QtCreatorSourceCodeAccess.Build.cs
@@ -22,7 +22,9 @@ namespace UnrealBuildTool.Rules
 	public class QtCreatorSourceCodeAccess : ModuleRules
 	{
 		public QtCreatorSourceCodeAccess(ReadOnlyTargetRules Target) : base(Target)
-		{			
+		{
+			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
 			PrivateDependencyModuleNames.AddRange(
 				new string[]
 				{
